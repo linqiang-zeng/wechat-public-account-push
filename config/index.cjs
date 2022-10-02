60a056bcb5ef4d18a6304107741741a50d47202f
linqiang-zeng <114865735+linqiang-zeng@users.noreply.github.com>
2022-10-02 23:39:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	4RQYE2SgZut_2kWlF2lb1Qv3WZKJ22prFIpKFj4CKcY',
+  CALLBACK_TEMPLATE_ID: '4RQYE2SgZut_2kWlF2lb1Qv3WZKJ22prFIpKFj4CKcY',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '4RQYE2SgZut_2kWlF2lb1Qv3WZKJ22prFIpKFj4CKcY',
+      id: 'oFk-O5y2FlEmSE_9pOh99u9hCAEY',
     }
   ],
 
