b6d789fd3ae1bd151a0923f7cf10ac87abbf7123
linqiang-zeng <114865735+linqiang-zeng@users.noreply.github.com>
2022-10-02 22:45:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小丹宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oFk-O5y2FlEmSE_9pOh99u9hCAEY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oFk-O5y2FlEmSE_9pOh99u9hCAEY',
+      useTemplateId: '	ZPAA21FJaB5o8t4DF3eWviBacUr8zdQBuaIW6v3n7-U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-07',
       festivals: [
